35aa293ce332554cb32f087a2f601a199669e73f
krohalova <50841954+krohalova@users.noreply.github.com>
2019-05-21 02:16:37 +0300

    doc

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -1,6 +1,13 @@
 /*
  * Decompiled with CFR 0.139.
  */
+/**
+ * @author krohalova
+ * @versin 1.0
+ *
+public class TitlesFrame
+extends JFrame {
+    /** Constructs a new TitlesFrame. */
 import java.awt.Component;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -12,9 +19,12 @@ extends JFrame {
     }
 
     private void initUI() {
+        /*Sets the title for this frame to the specified string
+        *@Param: title - the title to be displayed in the frame's border. A null value is treated as an empty string*/
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
         this.add(new TitlesPanel(94));
+        /*sets Size of the frame*/
         this.setSize(350, 350);
         this.setLocationRelativeTo(null);
     }
